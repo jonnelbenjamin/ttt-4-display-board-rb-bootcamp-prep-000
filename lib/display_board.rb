6acc7be09d2eed00f29e11a32c822a6b75abca95
Jonnel Benjamin <jonnel.benjamin@gmail.com>
2018-09-22 22:11:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,14 @@
 # out the current state.
 
 
-def display_board(board)
+# def display_board(board)
   
-  puts         "   |   |   "
-  puts         "-----------"
-  puts         "   |   |   "      
-  puts         "-----------"
-  puts         "   |   |   "
+#   puts         "   |   |   "
+#   puts         "-----------"
+#   puts         "   |   |   "      
+#   puts         "-----------"
+#   puts         "   |   |   "
   
-  end
+#   end
   
   
