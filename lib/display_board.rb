651462d5ed9c56ce7770891b3957842d76d77170
Jonnel Benjamin <jonnel.benjamin@gmail.com>
2018-09-22 22:12:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,15 @@
 # out the current state.
 
 
-# def display_board(board)
+def display_board(board)
   
-#   puts         "   |   |   "
-#   puts         "-----------"
-#   puts         "   |   |   "      
-#   puts         "-----------"
-#   puts         "   |   |   "
+  puts         "   |   |   "
+  puts         "-----------"
+  puts         "   |   |   "      
+  puts         "-----------"
+  puts         "   |   |   "
   
-#   end
+  end
 
 # def display_board(board)
 # separator = "|"
